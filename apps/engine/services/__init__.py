6eb256a9d65d070b2ab17d948862aee20d236440
naraharirohith <rohit.narahari@gmail.com>
2026-03-27 08:22:15 +0530

    Add engine news context service

--- apps/engine/services/__init__.py
+++ apps/engine/services/__init__.py
@@ -10,6 +10,7 @@ from .data_validator import validate_ohlcv, DataValidationError, get_data_qualit
 from .indicator_calculator import IndicatorCalculator
 from .backtester import run_backtest, run_backtest_on_df, run_backtest_multi
 from .asset_universe import ASSET_UNIVERSE, resolve_tickers
+from .news_fetcher import NewsFetcher
 from .walk_forward import run_walk_forward
 from .score_calculator import ScoreCalculator
 from .confidence_scorer import ConfidenceScorer
@@ -37,6 +38,7 @@ __all__ = [
     "run_backtest_multi",
     "ASSET_UNIVERSE",
     "resolve_tickers",
+    "NewsFetcher",
     "run_walk_forward",
     "ScoreCalculator",
     "ConfidenceScorer",
